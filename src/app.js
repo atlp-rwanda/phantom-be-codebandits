19f893a6ff4d96010a1367a2f9d842c814c98cc0
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-06-10 22:59:51 +0200

    Feat: Migrating to all frontend on same domain
    - Add the frontend files

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ app.use(cors(corsOptions));
 app.use(reqLogger);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(i18n.init);
 
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,20 +1,14 @@
 import { Router } from 'express';
+import path from 'path';
 import handleResponse from '../controllers/handleResponse.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	/* c8 ignore next 4 */
-	if (req.accepts()[0] === 'text/html') {
-		return res.redirect('/docs');
-	}
-	handleResponse(res, 200, res.__('welcome'));
-});
 router.all('/*', (req, res) => {
 	/* c8 ignore next 4 */
 	if (req.accepts()[0] === 'text/html') {
-		return res.status(404).send(res.__('notFound'));
+		return res.sendFile(path.join(path.resolve(), 'dist/index.html'));
 	}
-	return handleResponse(res, 404, res.__('notFound'));
+	handleResponse(res, 200, res.__('welcome'));
 });
 export default router;
